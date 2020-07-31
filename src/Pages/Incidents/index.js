1c12c47044cbb0989670c202cf4d95fbd5e492a4
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-07-30 21:17:08 -0300

    Second sctruture

--- src/Pages/Incidents/index.js
+++ src/Pages/Incidents/index.js
@@ -11,12 +11,26 @@ function Incidents() {
 
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
+  const [page , setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
   async function loadIncidents(){
-    const response = await api.get('incidents');
-    setIncidents(response.data);
+    if(loading){
+      return;
+    }
+    if(total > 0 && incidents.length === total){
+      return;
+    }
+    setLoading(true);
+    const response = await api.get('incidents',{
+      params: { page }
+    });
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(()=>{
@@ -43,7 +57,9 @@ function Incidents() {
       style={styles.incidentList}
       data={incidents}
       keyExtractor={incident => String(incident.id)}
-      showsVerticalScrollIndicator={false}
+      //showsVerticalScrollIndicator={false}
+      onEndReached={loadIncidents}
+      onEndReachedThreshold={0.2}
       renderItem={({ item: incident }) => (
         <View style={styles.incident}>
           <Text style={styles.incidentProperty}>ONG:</Text>
